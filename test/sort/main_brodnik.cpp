9b885f6dfebda1137b3d2b34d64621d3c8eb38b6
CharlesLakes <carlos.lagosc@usm.cl>
2025-03-09 21:54:35 -0300

    Comments added

--- test/sort/main_brodnik.cpp
+++ test/sort/main_brodnik.cpp
@@ -3,41 +3,49 @@
 
 using namespace std;
 
-void merge(brodnik_vector<int> &vec, int l, int r){
+void merge(brodnik_vector<int> &vec, int l, int r) {
     brodnik_vector<int> aux;
-    int mid = (l + r)/2;
-
+    int mid = (l + r) / 2;
     int i = l, j = mid + 1;
 
-    while(i <= mid && j <= r){
-        if(vec[i] <= vec[j]) aux.push_back(vec[i]), i++;
-        else aux.push_back(vec[j]), j++;
+    // Merge two sorted subarrays
+    while (i <= mid && j <= r) {
+        if (vec[i] <= vec[j]) aux.push_back(vec[i++]);
+        else aux.push_back(vec[j++]);
     }
 
-    while(i <= mid) aux.push_back(vec[i]), i++;
-    while(j <= r) aux.push_back(vec[j]), j++;
+    // Add remaining elements from left and right subarrays
+    while (i <= mid) aux.push_back(vec[i++]);
+    while (j <= r) aux.push_back(vec[j++]);
 
-    for(int i = l; i <= r; i++)
-        vec[i] = aux[i - l];
+    // Copy sorted elements back to the original array
+    for (int k = l; k <= r; k++)
+        vec[k] = aux[k - l];
 }
 
-void merge_sort(brodnik_vector<int> &vec, int l, int r){
-    if(l >= r) return;
-    int mid = (l + r)/2;
-    merge_sort(vec,l,mid);
-    merge_sort(vec,mid + 1,r);
-    merge(vec,l,r);
+void merge_sort(brodnik_vector<int> &vec, int l, int r) {
+    if (l >= r) return;
+    int mid = (l + r) / 2;
+
+    merge_sort(vec, l, mid);      // Sort left half
+    merge_sort(vec, mid + 1, r);  // Sort right half
+    merge(vec, l, r);             // Merge both halves
 }
 
-int main(){
-    int n; cin >> n;
+int main() {
+    int n;
+    cin >> n;
+
     brodnik_vector<int> vec(n);
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         cin >> vec[i];
 
-    merge_sort(vec,0,n-1);
+    merge_sort(vec, 0, n - 1);
 
-    for(int i = 0; i < n; i++)
-        cout << vec[i] << " "; cout << endl;
+    // Print sorted array
+    for (int i = 0; i < n; i++)
+        cout << vec[i] << " ";
+    cout << endl;
+
     return 0;
 }
