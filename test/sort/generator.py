beda3177ac14863013f27278f3ec689ad1e2bb2e
CharlesLakes <carlos.lagosc@usm.cl>
2025-03-10 09:54:48 -0300

    More test cases added

--- test/sort/generator.py
+++ test/sort/generator.py
@@ -14,7 +14,7 @@ def write_testcase(testcase_id, array):
 
 # T1
 # 10 testcase de arreglos con largo 10, con un interavlo de [1,100]
-for i in range(10):
+for i in range(20):
     array = [randint(1,100) for _ in range(10)]
 
     write_testcase(f"T1_{i}",array)
@@ -23,14 +23,14 @@ for i in range(10):
 
 # T2
 # 10 testcase de arreglos con largo 1000, con un interavlo de [1,10000]
-for i in range(10):
+for i in range(20):
     array = [randint(1,10000) for _ in range(1000)]
 
     write_testcase(f"T2_{i}",array)
 
 # T3
 # 10 testcase de arreglos con largo 100000, con un interavlo de [1,1000000000]
-for i in range(10):
+for i in range(20):
     array = [randint(1,1000000000) for _ in range(100000)]
 
     write_testcase(f"T3_{i}",array)
