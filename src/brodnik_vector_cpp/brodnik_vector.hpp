0d624633bb6387fa92686a59dffeac23cefe4392
CharlesLakes <carlos.lagosc@usm.cl>
2025-03-10 10:31:22 -0300

    CLANG format

--- src/brodnik_vector_cpp/brodnik_vector.hpp
+++ src/brodnik_vector_cpp/brodnik_vector.hpp
@@ -2,193 +2,178 @@
 #define BRODNIK_VECTOR_HPP
 #include <iostream>
 
-template <class T>
-class brodnik_vector{
-    private:
-        int n_size;
+template <class T> class brodnik_vector {
+private:
+  int n_size;
 
-        // super block
-        int sb_index; // s - 1
-        int sb_size;
-        int sb_max_size;
+  // super block
+  int sb_index; // s - 1
+  int sb_size;
+  int sb_max_size;
 
-        // data block
-        int db_index; // d - 1
-        int db_size;
-        int db_max_size;
-        
-        // index block
-        int ib_size;
-        int ib_max_size;
-        T **index_block;
-        
-        void init();
+  // data block
+  int db_index; // d - 1
+  int db_size;
+  int db_max_size;
 
-        void grow();
-        void shrink();
-        T& locate(int i);
+  // index block
+  int ib_size;
+  int ib_max_size;
+  T **index_block;
 
-    public:
-        brodnik_vector();
-        brodnik_vector(int n);
+  void init();
 
-        ~brodnik_vector();
-        
-        
-        void push_back(T value);
-        
-        void pop_back();
+  void grow();
+  void shrink();
+  T &locate(int i);
 
-        T& operator[](int index);
+public:
+  brodnik_vector();
+  brodnik_vector(int n);
 
+  ~brodnik_vector();
+
+  void push_back(T value);
+
+  void pop_back();
+
+  T &operator[](int index);
 };
 
 #define fast_log2(x) (31 - __builtin_clz(x))
 
+template <class T> brodnik_vector<T>::brodnik_vector() { init(); }
 
-template <class T>
-brodnik_vector<T>::brodnik_vector(){
-    init();
+template <class T> brodnik_vector<T>::brodnik_vector(int n) {
+  init();
+  while (n--)
+    this->grow();
 }
 
-template <class T>
-brodnik_vector<T>::brodnik_vector(int n){
-    init();
-    while(n--) this->grow();
+template <class T> void brodnik_vector<T>::init() {
+  this->db_size = 0;
+  this->sb_size = 1;
+
+  this->db_max_size = this->sb_max_size = 1;
+
+  this->db_index = this->sb_index = 0;
+
+  this->ib_size = 1;
+  this->ib_max_size = 1;
+
+  this->index_block = new T *[1];
+  this->index_block[0] = new T[1];
+
+  this->n_size = 0;
 }
 
-template <class T>
-void brodnik_vector<T>::init(){
-    this->db_size = 0;
-    this->sb_size = 1;
-
-    this->db_max_size = this->sb_max_size = 1;
-    
-    this->db_index = this->sb_index = 0;
-
-    this->ib_size = 1;
-    this->ib_max_size = 1;
-
-    this->index_block = new T*[1];
-    this->index_block[0] = new T[1];
-    
-    this->n_size = 0;
+template <class T> brodnik_vector<T>::~brodnik_vector() {
+  for (int i = 0; i < this->ib_size; i++) {
+    if (this->index_block[i] == nullptr)
+      continue;
+    delete[] this->index_block[i];
+  }
+  if (this->index_block != nullptr)
+    delete[] this->index_block;
 }
 
-template <class T>
-brodnik_vector<T>::~brodnik_vector(){
-    for(int i = 0; i < this->ib_size; i++){
-        if(this->index_block[i] == nullptr) continue;
-        delete[] this->index_block[i];
-    }
-    if(this->index_block != nullptr) delete[] this->index_block;
-}
+template <class T> void brodnik_vector<T>::grow() {
+  if (this->db_size == this->db_max_size) {
 
-template <class T>
-void brodnik_vector<T>::grow(){
-    if(this->db_size == this->db_max_size){
-
-        if(this->sb_size == this->sb_max_size){
-            this->sb_index++; // Increment s
-            if(this->sb_index % 2) this->db_max_size *= 2; // s even
-            else this->sb_max_size *= 2; // s odd
-            this->sb_size = 0;
-            
-        }
-
-        if(this->db_index + 2 > this->ib_size){
-            if(this->ib_size == this->ib_max_size){
-
-                // TODO: utilizar realloc y abstraerlo
-                T **new_index_block = new T*[this->ib_size + 1];
-                for(int i = 0; i < this->ib_size; i++) new_index_block[i] = this->index_block[i];
-                
-                delete[] this->index_block;
-                this->index_block = nullptr;
-                this->index_block = new_index_block;
-
-                this->ib_max_size++;
-            }
-            this->index_block[this->ib_size] = new T[this->db_max_size];
-            this->ib_size++;
-        }
-
-        this->db_index++;
-        this->db_size = 0;
-        
-        this->sb_size++;
+    if (this->sb_size == this->sb_max_size) {
+      this->sb_index++; // Increment s
+      if (this->sb_index % 2)
+        this->db_max_size *= 2; // s even
+      else
+        this->sb_max_size *= 2; // s odd
+      this->sb_size = 0;
     }
 
-    
-    this->db_size++;
-    this->n_size++;
-}
-
-template <class T>
-void brodnik_vector<T>::shrink(){
-    this->n_size--;
-    this->db_size--;
-    
-    if(!this->db_size){
-        if(this->db_index + 2 == this->ib_size){
-            delete[] this->index_block[this->db_index + 1];
-            this->index_block[this->db_index + 1] = nullptr;
-        }
+    if (this->db_index + 2 > this->ib_size) {
+      if (this->ib_size == this->ib_max_size) {
 
         // TODO: utilizar realloc y abstraerlo
-        if(this->ib_size*4 <= this->ib_max_size){
-            this->ib_max_size = this->ib_size;
-            T **new_index_block = new T*[this->ib_size];
-            for(int i = 0; i < this->ib_size; i++)
-                new_index_block[i] = this->index_block[i];
-            
-            delete[] this->index_block;
-            this->index_block = nullptr;
-            this->index_block = new_index_block;
-        }
+        T **new_index_block = new T *[this->ib_size + 1];
+        for (int i = 0; i < this->ib_size; i++)
+          new_index_block[i] = this->index_block[i];
 
-        this->db_index--;
-        this->sb_size--;
-        
-        if(!this->sb_size){
-            this->sb_index--;
-            if(this->sb_index % 2) this->db_max_size /= 2;
-            else this->sb_max_size /= 2;
-            this->sb_size = this->sb_max_size;
-        }
+        delete[] this->index_block;
+        this->index_block = nullptr;
+        this->index_block = new_index_block;
+
+        this->ib_max_size++;
+      }
+      this->index_block[this->ib_size] = new T[this->db_max_size];
+      this->ib_size++;
     }
+
+    this->db_index++;
+    this->db_size = 0;
+
+    this->sb_size++;
+  }
+
+  this->db_size++;
+  this->n_size++;
 }
 
-template <class T>
-T& brodnik_vector<T>::locate(int i){
-    int r = i + 1;
-    int k = fast_log2(i+1);
-    int b = (r >> ((k+1)/2)) - (1 << (k/2));
-    int e = (r & ((1 << ((k+1)/2)) - 1));
-    int p = (1 << ((k+1)/2)) + (1 << ((k)/2)) - 2;
-    return this->index_block[p+b][e];
+template <class T> void brodnik_vector<T>::shrink() {
+  this->n_size--;
+  this->db_size--;
+
+  if (!this->db_size) {
+    if (this->db_index + 2 == this->ib_size) {
+      delete[] this->index_block[this->db_index + 1];
+      this->index_block[this->db_index + 1] = nullptr;
+    }
+
+    // TODO: utilizar realloc y abstraerlo
+    if (this->ib_size * 4 <= this->ib_max_size) {
+      this->ib_max_size = this->ib_size;
+      T **new_index_block = new T *[this->ib_size];
+      for (int i = 0; i < this->ib_size; i++)
+        new_index_block[i] = this->index_block[i];
+
+      delete[] this->index_block;
+      this->index_block = nullptr;
+      this->index_block = new_index_block;
+    }
+
+    this->db_index--;
+    this->sb_size--;
+
+    if (!this->sb_size) {
+      this->sb_index--;
+      if (this->sb_index % 2)
+        this->db_max_size /= 2;
+      else
+        this->sb_max_size /= 2;
+      this->sb_size = this->sb_max_size;
+    }
+  }
 }
 
-
-template <class T>
-void brodnik_vector<T>::push_back(T value){
-    this->grow();
-
-    T& current_cell = this->locate(this->n_size - 1);
-
-    current_cell = value;
+template <class T> T &brodnik_vector<T>::locate(int i) {
+  int r = i + 1;
+  int k = fast_log2(i + 1);
+  int b = (r >> ((k + 1) / 2)) - (1 << (k / 2));
+  int e = (r & ((1 << ((k + 1) / 2)) - 1));
+  int p = (1 << ((k + 1) / 2)) + (1 << ((k) / 2)) - 2;
+  return this->index_block[p + b][e];
 }
 
+template <class T> void brodnik_vector<T>::push_back(T value) {
+  this->grow();
 
-template <class T>
-void brodnik_vector<T>::pop_back(){
-    this->shrink();
+  T &current_cell = this->locate(this->n_size - 1);
+
+  current_cell = value;
 }
 
+template <class T> void brodnik_vector<T>::pop_back() { this->shrink(); }
 
-template <class T>
-T& brodnik_vector<T>::operator[](int index){
-    return this->locate(index);
+template <class T> T &brodnik_vector<T>::operator[](int index) {
+  return this->locate(index);
 }
 
 #endif
--- test/sort/main_brodnik.c
+++ test/sort/main_brodnik.c
@@ -1,65 +1,66 @@
-#include <stdio.h>
 #include "../../src/brodnik_vector_c/brodnik_vector.c"
+#include <stdio.h>
 
 void merge(brodnik_vector *vec, int l, int r) {
-    brodnik_vector aux;
-    init(&aux);
-    int mid = (l + r) / 2;
-    int i = l, j = mid + 1;
+  brodnik_vector aux;
+  init(&aux);
+  int mid = (l + r) / 2;
+  int i = l, j = mid + 1;
 
-    // Merge two sorted subarrays
-    while (i <= mid && j <= r) {
-        if ((*locate(vec,i)) <= (*locate(vec,j))) 
-            push_back(&aux,*locate(vec,i++));
-        else 
-            push_back(&aux,*locate(vec,j++));
-    }
+  // Merge two sorted subarrays
+  while (i <= mid && j <= r) {
+    if ((*locate(vec, i)) <= (*locate(vec, j)))
+      push_back(&aux, *locate(vec, i++));
+    else
+      push_back(&aux, *locate(vec, j++));
+  }
 
-    // Add remaining elements from left and right subarrays
-    while (i <= mid) push_back(&aux,*locate(vec,i++));
-    while (j <= r) push_back(&aux,*locate(vec,j++));
+  // Add remaining elements from left and right subarrays
+  while (i <= mid)
+    push_back(&aux, *locate(vec, i++));
+  while (j <= r)
+    push_back(&aux, *locate(vec, j++));
 
-    // Copy sorted elements back to the original array
-    for (int k = l; k <= r; k++){
-        int *ptr_value = locate(vec,k);
-        *ptr_value = *locate(&aux,k - l);
-    }
+  // Copy sorted elements back to the original array
+  for (int k = l; k <= r; k++) {
+    int *ptr_value = locate(vec, k);
+    *ptr_value = *locate(&aux, k - l);
+  }
 
-    delete(&aux);
+  delete (&aux);
 }
 
 void merge_sort(brodnik_vector *vec, int l, int r) {
-    if (l >= r) return;
-    int mid = (l + r) / 2;
+  if (l >= r)
+    return;
+  int mid = (l + r) / 2;
 
-    merge_sort(vec, l, mid);      // Sort left half
-    merge_sort(vec, mid + 1, r);  // Sort right half
-    merge(vec, l, r);             // Merge both halves
+  merge_sort(vec, l, mid);     // Sort left half
+  merge_sort(vec, mid + 1, r); // Sort right half
+  merge(vec, l, r);            // Merge both halves
 }
 
-int main(){
-    brodnik_vector vec;
+int main() {
+  brodnik_vector vec;
 
-    init(&vec);
+  init(&vec);
 
-    int n;
-    scanf("%d", &n);
+  int n;
+  scanf("%d", &n);
 
-    for(int i = 0; i < n; i++){
-        int x;
-        scanf("%d", &x);
-        push_back(&vec,x);
-    }
+  for (int i = 0; i < n; i++) {
+    int x;
+    scanf("%d", &x);
+    push_back(&vec, x);
+  }
 
-    merge_sort(&vec,0,n-1);
+  merge_sort(&vec, 0, n - 1);
 
-    for(int i = 0; i < n; i++){
-        printf("%d ",*locate(&vec,i));
-    }
+  for (int i = 0; i < n; i++) {
+    printf("%d ", *locate(&vec, i));
+  }
 
-    delete(&vec);
+  delete (&vec);
 
-
-    return 0;
+  return 0;
 }
-
