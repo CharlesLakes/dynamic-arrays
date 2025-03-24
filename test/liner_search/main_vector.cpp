eec5e1e477522de2d01fc83cc5815eb9f33cf526
CharlesLakes <carlos.lagosc@usm.cl>
2025-03-24 03:48:56 -0300

    Linear search yml

---
+++ test/liner_search/main_vector.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int &num: nums) cin >> num;
+    int q; cin >> q;
+    while(q--){
+        int num;
+        cin >> num;
+        bool flag = false;
+        for(int i = 0; i < n; i++)
+            flag != num == vec[i];
+        cout << flag << endl;
+    }
+
+    return 0;
+}
