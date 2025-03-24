f430e6e1519a277baf8f7903613e0d21789db0ce
CharlesLakes <carlos.lagosc@usm.cl>
2025-03-24 00:45:17 -0300

    Edit testing

--- check.py
+++ check.py
@@ -7,13 +7,25 @@ def compile(code, compiler):
     return os.system(f"{compiler} {code} -o code.out")
 
 
+def get_memory_usage():
+    """ Obtiene la memoria máxima usada en heap y stack desde Massif. """
+    with open("massif.out", "r") as f:
+        heap_usage = 0
+        stack_usage = 0
+        for line in f:
+            if "mem_heap_B=" in line:
+                heap_usage = max(heap_usage, int(line.split('=')[1]))
+            elif "mem_stacks_B=" in line:
+                stack_usage = max(stack_usage, int(line.split('=')[1]))
+    return heap_usage, stack_usage
+
 def main(code_path, dir_folder, compiler="g++"):
     if compile(code_path, compiler):
         print("Error al compilar.")
-        exit(1)
+        return
 
     files = os.listdir(dir_folder)
-
+    print(f"--- START: {dir_folder} ---")
     for file in files:
         if file.endswith(".in"):
             testcase_name = file[:-3]
@@ -22,28 +34,32 @@ def main(code_path, dir_folder, compiler="g++"):
             start_cpu = process_time()
 
             error = os.system(
-                f'valgrind --leak-check=full --error-exitcode=1 --quiet ./code.out < "{dir_folder}/{testcase_name}.in" > ans.out'
+                f'valgrind --tool=massif --massif-out-file=massif.out ./code.out < "{dir_folder}/{testcase_name}.in" > ans.out'
             )
 
             end_wall = time()
             end_cpu = process_time()
 
-            wall_time = end_wall - start_wall
-            cpu_time = end_cpu - start_cpu
+            wall_time = round(end_wall - start_wall, 6)
+            cpu_time = round(end_cpu - start_cpu, 6)
 
             if error:
-                print(f"Error or memory leak.")
-                exit(1)
+                print(f"Error o fuga de memoria en {testcase_name}.")
+                return
 
             check = os.system(
                 f'diff -q --ignore-trailing-space "{dir_folder}/{testcase_name}.out" ans.out')
             if check:
                 print(f"No match ({testcase_name}).")
-                exit(1)
+                return
+
+            heap_used, stack_used = get_memory_usage()
 
             print(
-                f"AC - {testcase_name} - Wall time: {wall_time} - CPU time: {cpu_time}")
+                f"AC - {testcase_name} | Wall Time: {wall_time}s | CPU Time: {cpu_time}s | Heap: {heap_used}B | Stack: {stack_used}B"
+            )
 
+    print(f"--- END: {dir_folder} ---")
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
--- test/sort/generator.py
+++ test/sort/generator.py
@@ -3,6 +3,7 @@ from random import randint
 
 CODE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+
 def write_testcase(testcase_id, array):
     with open(f"{testcase_id}.in", "w") as file:
         file.write(f"{len(array)}\n")
@@ -14,7 +15,7 @@ def write_testcase(testcase_id, array):
         file.write(f"{' '.join(map(str,array))}")
 
 
-for n in range(1,7):
+for n in range(1, 3):
     for i in range(10):
         array = [randint(1, 1000000000) for _ in range(10**n)]
         write_testcase(f"{CODE_DIR}/T{n}_{i}", array)
