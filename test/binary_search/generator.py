5d4cc4c035073394b0720ad1b4204c2269111a42
CharlesLakes <carlos.lagosc@usm.cl>
2025-04-14 23:10:49 -0400

    Change

--- test/binary_search/generator.py
+++ test/binary_search/generator.py
@@ -19,7 +19,7 @@ def write_testcase(testcase_id, array, queries):
         file.write(f"{' '.join(map(str,answer))}")
 
 
-for n in range(1, 4 if debug_flag else 10):
+for n in range(1, 3 if debug_flag else 10):
     for i in range(10 if debug_flag else 1):
         array = [randint(1, 10**n) for _ in range(10**n)]
         array.sort()
--- test/sort/generator.py
+++ test/sort/generator.py
@@ -16,7 +16,7 @@ def write_testcase(testcase_id, array):
         file.write(f"{' '.join(map(str,array))}")
 
 
-for n in range(1, 4 if debug_flag else 10):
+for n in range(1, 3 if debug_flag else 10):
     for i in range(10 if debug_flag else 1):
         array = [randint(1, 1000000000) for _ in range(10**n)]
         write_testcase(f"{CODE_DIR}/T{n}_{i}", array)
