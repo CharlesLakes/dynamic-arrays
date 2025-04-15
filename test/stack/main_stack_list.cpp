c8e05a33098f6969bbbbffb73ef09610856e105a
CharlesLakes <carlos.lagosc@usm.cl>
2025-04-14 23:50:49 -0400

    feat(stack): STL multi container

---
+++ test/stack/main_stack_list.cpp
@@ -0,0 +1,42 @@
+#include "../debug.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    stack<int, list<int>> st;
+    
+    int number_of_element;
+    cin >> number_of_element;
+    
+    while(number_of_element--){
+        int number; 
+        cin >> number;
+        
+        st.push(number);
+    }
+
+    int queries;
+    cin >> queries;
+    while(queries--){
+        int operation; 
+        cin >> operation;
+        
+        if(operation){
+            int value; 
+            cin >> value;
+            
+            st.push(value);
+        }else{
+            st.pop();
+        }
+    }
+
+    while(!st.empty()){
+        PRINT_INT(st.top());
+        st.pop();
+    }
+
+    PRINT_CHECK();
+    return 0;
+}
