3c0c1149662e7c4b385caa41654f5c0ccec5bc6b
CharlesLakes <carlos.lagosc@usm.cl>
2025-04-14 23:18:12 -0400

    Change

--- test/binary_search/generator.py
+++ test/binary_search/generator.py
@@ -20,7 +20,7 @@ def write_testcase(testcase_id, array, queries):
 
 
 for n in range(1, 3 if debug_flag else 10):
-    for i in range(10 if debug_flag else 1):
+    for i in range(2 if debug_flag else 1):
         array = [randint(1, 10**n) for _ in range(10**n)]
         array.sort()
         queries = [randint(1, 10**n) for _ in range(10**n)]
